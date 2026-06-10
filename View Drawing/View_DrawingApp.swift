ab117204c7fde94b286d0642368805e3cf08a772
Naveen Gaur <naveengaur800@gmail.com>
2026-06-10 23:39:13 +1000

    Initial Commit

---
+++ View Drawing/View_DrawingApp.swift
@@ -0,0 +1,17 @@
+//
+//  View_DrawingApp.swift
+//  View Drawing
+//
+//  Created by Naveen Gaur on 10/6/2026.
+//
+
+import SwiftUI
+
+@main
+struct View_DrawingApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
